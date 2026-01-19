099d8d69937a23b1921d15b5e0689d2097137158
nathomp3 <nathomp3@ncsu.edu>
2026-01-19 18:16:58 -0500

    Replace fluent-ffmpeg

--- src/downloadSongs.js
+++ src/downloadSongs.js
@@ -1,7 +1,7 @@
 import { readFileSync, existsSync } from 'fs'
 import PQueue from 'p-queue';
-import Ffmpeg from 'fluent-ffmpeg';
 import { getFirstAnime, getFirstAnimeId } from './util.js'
+import { exec } from 'child_process';
 
 export async function downloadSongs() {
   const queue = new PQueue({
@@ -22,24 +22,30 @@ function downloadSong(song, url, id, attempt = 0) {
     const anime = getFirstAnime(song);
     const annId = getFirstAnimeId(song);
     
-    let download = Ffmpeg(url);
+    let command = [
+      'ffmpeg', 
+      '-i', `${url}`,
+      '-y'
+    ];
+
     if(existsSync(`./data/art/${annId}.jpg`)) {
-      download = download.addOutputOptions('-i', `./data/art/${annId}.jpg`, '-map', '0:a:0', '-map', '1:0', '-c:a', 'libmp3lame', '-c:v', 'mjpeg', '-id3v2_version', '3')
+      command.push('-i', `./data/art/${annId}.jpg`, '-map', '0:a:0', '-map', '1:0', '-c:a', 'libmp3lame', '-c:v', 'mjpeg', '-id3v2_version', '3')
     }
-    download
-      .addOutputOption('-metadata', `Title=${song.name}`)
-      .addOutputOption('-metadata', `Artist=${song.artist.replace(/\//g, '\\')}`)
-      .addOutputOption('-metadata', `Album=${anime.names.EN ?? anime.names.JA}`)
-      .addOutputOption('-metadata', `Album_Artist=Various`)
-      .addOutputOption('-map_chapters', '-1')
-      .addOutputOption('-ab', '128k')
-      .addOutputOption('-ar', '44100')
-      .outputFormat('mp3')
-      .save(`./data/songs/${id}.mp3`)
-      .on('end', function() {
-        resolve();
-      })
-      .on('error', function() {
+
+    command.push(
+      '-metadata', `Title="${escapeQuotes(song.name)}"`,
+      '-metadata', `Artist="${escapeQuotes(song.artist.replace(/\//g, '\\'))}"`,
+      '-metadata', `Album="${escapeQuotes(anime.names.EN ?? anime.names.JA)}"`,
+      '-metadata', `Album_Artist=Various`,
+      '-map_chapters', '-1',
+      '-ab', '128k',
+      '-ar', '44100',
+      '-f', 'mp3',
+      `./data/songs/${id}.mp3`
+    );
+
+    exec(command.join(' '), (err) => {
+      if(err) {
         if(attempt >= 5) {
           console.log(`Failed to download ${url}`);
           resolve();
@@ -47,6 +53,13 @@ function downloadSong(song, url, id, attempt = 0) {
         }
         console.log(`Retrying ${url}`)
         downloadSong(song, url, id, attempt + 1).then(() => resolve());
-      })
-  })
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function escapeQuotes(str) {
+  return str.replaceAll('"', '\\"')
 }
